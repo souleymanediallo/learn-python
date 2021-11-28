df181da6dcffa66d2fa020012981d31c98201f52
souleymane <souleymanedialloo@yahoo.fr>
2021-11-28 12:24:08 +0100

    finish snake game

--- chap_12/app_2.py
+++ chap_12/app_2.py
@@ -1,15 +1,27 @@
 from turtle import Turtle, Screen
 import random
 
+tomy = Turtle()
+tomy.pencolor("#33cc8c")
+
+
 colors = ["red", "green", "pink", "blue", "red", "green", "pink", "blue"]
 angle_turtle = [0, 90, 180, 270]
 
-tomy = Turtle()
+
+def random_colors():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
 tomy.pensize(10)
 tomy.speed("fastest")
 
 for _ in range(200):
-    tomy.color(random.choice(colors))
+    tomy.color(random_colors())
     tomy.forward(30)
     tomy.setheading(random.choice(angle_turtle))
 
